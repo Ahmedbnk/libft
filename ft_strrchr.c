278c0570caf64cdd1557798c7fd48d4ab82c8be1
Ahmed Benkrara <abenkrar@c2r9p2.1337.ma>
2025-02-05 15:20:52 +0100

    commited

---
+++ ft_strrchr.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abenkrar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/22 21:11:04 by abenkrar          #+#    #+#             */
+/*   Updated: 2024/11/14 16:38:57 by abenkrar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	int	i;
+	int	c_index;
+
+	if (s == NULL)
+		return (NULL);
+	i = 0;
+	c_index = -1;
+	while (s[i] != '\0')
+	{
+		if (s[i] == (char)c)
+		{
+			c_index = i;
+		}
+		i++;
+	}
+	if (c_index != -1)
+	{
+		return ((char *)(s + c_index));
+	}
+	if (c == '\0')
+	{
+		return ((char *)(s + i));
+	}
+	return (NULL);
+}
